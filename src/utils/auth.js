6d2850df628ad2e24da6f8f5bec8c06da27e3932
yongtao1998 <zhangyongtao1310@yeah.net>
2020-03-15 17:54:36 +0800

    token组件封装

---
+++ src/utils/auth.js
@@ -0,0 +1,22 @@
+/***
+ * auth.js 专门处理token读写
+ * create by zyt 2020/2/11
+ *
+ */
+
+const USER_TOKEN = 'toutiao-move'
+
+// 设置token
+export function setUser (user) {
+  localStorage.setItem(USER_TOKEN, JSON.stringify(user))
+}
+
+// 获取用户的token
+export function getUser () {
+  return JSON.parse(localStorage.getItem(USER_TOKEN) || '{}')
+}
+
+// 删除用户的token
+export function delUser () {
+  localStorage.removeItem(USER_TOKEN)
+}
