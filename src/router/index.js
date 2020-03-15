33bd8680c27e809cd143c05e5585337e6ea58e58
yongtao1998 <zhangyongtao1310@yeah.net>
2020-03-15 16:54:32 +0800

    路由挂载

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,51 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '@/views/home'
 Vue.use(VueRouter)
+const Layout = () => import('@/views/layout')
+const home = () => import('@/views/home')
+const question = () => import('@/views/question')
+const video = () => import('@/views/video')
 
 const routes = [
   {
     path: '/',
-    component: Home
+    component: Layout,
+    children: [
+      {
+        // 默认为二级路由的默认组件
+        path: '',
+        component: home
+      },
+      {
+        path: '/question',
+        component: question // 问答组件
+      }, {
+        path: '/video',
+        component: video // 视频组件
+      }, {
+        path: '/user',
+        component: () => import('@/views/user') // 用户
+      }
+    ]
+  }, {
+    path: '/login',
+    component: () => import('@/views/login')
+  },
+  {
+    path: '/user/profile',
+    component: () => import('@/views/user/profile')
+  },
+  {
+    path: '/search',
+    component: () => import('@/views/search')
+  },
+  {
+    path: '/search/result',
+    component: () => import('@/views/search/result')
+  },
+  {
+    path: '/article',
+    component: () => import('@/views/article')
   }
 
 ]
---
+++ src/views/search/result.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>查询结果</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
