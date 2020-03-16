7491978046c69d47ba771bde525d9f84275b9b1e
yongtao1998 <zhangyongtao1310@yeah.net>
2020-03-16 16:57:48 +0800

    vuex管理token

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import * as auth from '@/utils/auth'
+import * as auth from '@/utils/auth' // 别名配置
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    // token 信息的对象
     user: auth.getUser()
   },
+  // 修改token
   mutations: {
+    // 修改token
+    updateUser (state, payload) {
+      // 将 payload 中的token数据 给 state
+      state.user = payload.user
+      // 存入本地
+      auth.setUser(payload.user)
+    },
+    // 删除token
+    deltoken (state) {
+      // 清除 state中的token
+      state.user = {}
+      // 清除本地的token
+      auth.delUser()
+    }
+
   },
   actions: {
   },
