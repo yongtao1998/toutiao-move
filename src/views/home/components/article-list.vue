660e07dc3281c5d6ee0d8515ed35599fd8ebef12
yongtao1998 <zhangyongtao1310@yeah.net>
2020-03-18 18:56:28 +0800

    tab导航、文章列表组件封装

---
+++ src/views/home/components/article-list.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- 文章列表组件 放置列表 -->
+  <!-- van-list 可以帮助我们实现上拉加载 -->
+  <!-- 预留 阅读记忆 -->
+  <div class="scroll-wrapper">
+      <!-- loading 和 finished 两个变量控制加载状态 -->
+       <!-- 当组件 滚动到底部 会触发 load事件 把loading 修改为 true 此时会触发异步操作并更新数据
+            数据更新完 将loading设置为 false 就可以
+            注意：van-list 初始化完毕 就会检测距离底部的长度，如果超过30px 就会将 loading 修改为 true
+        -->
+      <van-list v-model="loading" :finished="finished" @load="onLoad" finished-text="没有更多了">
+
+      </van-list>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      loading: false, // 是否开启上拉加载
+      finished: false // 是否完成所有数据的加载
+    }
+  },
+  methods: {
+    // 异步操作 获取数据 并更新
+    onLoad () {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,22 +1,86 @@
 <template>
-  <div>
-   <div class="split"></div>
-    <van-skeleton title avatar :row="3" />
-    <div class="split"></div>
-    <van-skeleton title avatar :row="3" />
-    <div class="split"></div>
-    <van-skeleton title avatar :row="3" />
-    <div class="split"></div>
-    <van-skeleton title avatar :row="3" />
-  </div>
+
+<div class="container">
+<!-- 放置 tabs组件 -->
+<van-tabs>
+  <!-- title值为当前显示的标签页 -->
+  <van-tab :title="`标签${item}`" v-for="item in 10" :key="item">
+    <!-- 文章列表  -->
+    <ArticleList></ArticleList>>
+  </van-tab>
+</van-tabs>
+
+<!-- tabs下 放置 图标 编辑 频道的图标 -->
+<span class="bar_btn">
+  <!-- vant图标 -->
+  <van-icon name="wap-nav" />
+</span>
+</div>
 </template>
 
 <script>
+import ArticleList from './components/article-list'
 export default {
-
+  components: {
+    ArticleList
+  }
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content{
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane{
+    height: 100%;
+    .scroll-wrapper{
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
+}
 </style>
